424751a14c38aa1e595d5adf7581817ea66f049b
Hajin <hajinkim@kangwon.ac.kr>
2019-05-26 20:17:27 +0900

    chapter6.4 리플렉션 API를 이용하여 프런트 컨트롤러 개선하기

--- src/main/java/controls/LogInController.java
+++ src/main/java/controls/LogInController.java
@@ -1,12 +1,13 @@
 package controls;
 
+import bind.DataBinding;
 import dao.MemberDao;
 import vo.Member;
 
 import javax.servlet.http.HttpSession;
 import java.util.Map;
 
-public class LogInController implements Controller {
+public class LogInController implements Controller, DataBinding {
     MemberDao memberDao;
 
     public LogInController setMemberDao(MemberDao memberDao) {
@@ -16,16 +17,27 @@ public class LogInController implements Controller {
 
     @Override
     public String execute(Map<String, Object> model) throws Exception {
-        if (model.get("email") != null) {
-            Member member = memberDao.exist((String) model.get("email"), (String) model.get("password"));
+        Member loginInfo = (Member) model.get("loginInfo");
+        if (loginInfo.getEmail() == null) { // 입력폼을 요청할 때
+            return "/WEB-INF/views/auth/LogInForm.jsp";
+
+        } else { // 회원 등록을 요청할 때
+            Member member = memberDao.exist(loginInfo.getEmail(), loginInfo.getPassword());
+
             if (member != null) {
-                HttpSession session = (HttpSession)model.get("session");
+                HttpSession session = (HttpSession) model.get("session");
                 session.setAttribute("member", member);
                 return "redirect:../member/list.do";
             } else {
                 return "/WEB-INF/views/auth/LogInFail.jsp";
             }
         }
-         return "/WEB-INF/views/auth/LogInForm.jsp";
+    }
+
+    @Override
+    public Object[] getDataBinders() {
+        return new Object[] {
+                "loginInfo", vo.Member.class
+        };
     }
 }
